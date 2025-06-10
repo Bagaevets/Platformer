496f289eb23800f1b959419d965fd3a88ddf9dee
Bagayevets <ka4ergd@gmail.com>
2025-06-10 21:49:44 +0300

    add unity project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+//прекол
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>(); //Возьмет компонент Rigidbody у прикреп. объекта       
+    }
+
+    private void Update()
+    {
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal"),body.linearVelocity.y);   
+    }
+
+}   
