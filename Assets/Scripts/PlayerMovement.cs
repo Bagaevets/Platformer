59f38fda73984966c5c86718c5d82df8f895377f
Bagayevets <ka4ergd@gmail.com>
2025-06-13 16:43:07 +0300

    Добавил прыжок, повороты, ассет игрока, анимацию бега и спокойствия

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,35 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
-//прекол
+
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator animator;
 
-    private void Awake() // Hey
+    private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>(); //Возьмет компонент Rigidbody у прикреп. объекта       
+        animator = GetComponent<Animator>();
     }
-
     private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal"),body.linearVelocity.y);   
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.linearVelocity = new Vector2(horizontalInput * speed,body.linearVelocity.y);
+
+        //Flip player when moving left-right
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1,1,1);
+
+        if (Input.GetKey(KeyCode.Space)) // Проверяет Нажатие клавиши
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+
+        //Set animator parameters
+        animator.SetBool("Run", horizontalInput != 0);
     }
 
 }   
