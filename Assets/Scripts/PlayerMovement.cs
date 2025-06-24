2c48789c249f30dc1e072001c4497d2ad465ad12
Bagayevets <ka4ergd@gmail.com>
2025-06-24 18:37:22 +0300

    Атакую

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -86,9 +86,6 @@ public class PlayerMovement : MonoBehaviour
             
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-    }
 
     private bool isGronded()
     {
@@ -100,5 +97,10 @@ public class PlayerMovement : MonoBehaviour
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
+    }
+
+    public bool canAttack()
+    { 
+        return horizontalInput  == 0 && isGronded() && !onWall();
     }
 }   
