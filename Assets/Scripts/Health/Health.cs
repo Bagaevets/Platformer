c05e3256bf80e64f07408ac6059fd856329f15c4
Bagayevets <ka4ergd@gmail.com>
2025-07-21 23:48:51 +0300

    Добавил дальних и ближних врагов

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -15,6 +15,9 @@ public class Health : MonoBehaviour
     [SerializeField] private int numberOfFlashes;
     private SpriteRenderer spriteRend;
 
+    [Header("Components")]
+    [SerializeField] private Behaviour[] components;
+
     private void Awake()
     {
         currentHealth = startingHealth;
@@ -36,7 +39,8 @@ public class Health : MonoBehaviour
             if (!dead)
             {
                 anim.SetTrigger("die");
-                GetComponent<PlayerMovement>().enabled = false;
+                foreach (Behaviour component in components)
+                    component.enabled = false;
                 dead = true;
             }
         }
@@ -57,6 +61,11 @@ public class Health : MonoBehaviour
             yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
         }
         Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
+
+    private void Deactivate()
+    {
+       gameObject.SetActive(false);
     }
 }
 
